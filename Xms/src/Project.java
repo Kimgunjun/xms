5c16ff79ded04abfd7dead3d596ff05882a9ae80
kimgunjun <127161967+Kimgunjun@users.noreply.github.com>
2023-03-22 23:20:30 +0900

    Test

---
+++ Xms/src/Project.java
@@ -0,0 +1,13 @@
+
+public class Project {
+
+	public Project() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");// TODO Auto-generated method stub
+
+	}
+
+}
