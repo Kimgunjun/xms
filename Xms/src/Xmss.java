0afff9b030c55bbaf45941ea808c61d9761b24d0
kimgunjun <127161967+Kimgunjun@users.noreply.github.com>
2023-03-22 23:59:36 +0900

    test

--- Xms/src/Xmss.java
+++ Xms/src/Xmss.java
@@ -6,6 +6,7 @@ public class Xmss {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("Hello, world!");
 		System.out.println("Hello, world!");// TODO Auto-generated method stub
 
 	}
