e031955cbaa62b18add299850aefb7a54bcdd0aa
kimgunjun <127161967+Kimgunjun@users.noreply.github.com>
2023-03-23 20:21:21 +0900

    2-2

--- Xms/src/Xmss.java
+++ Xms/src/Xmss.java
@@ -1,20 +1,12 @@
 public class Xmss {
 
 	public Xmss() {
-		
 	}
-
 	public static void main(String[] args) {
-		int i = 100000; /* 100000s를 정수로 나타내었다. */
-		double D = 31536000;	 
-				double H = 86400;
-						double M = 3600;
-								int S = 1;
-				System.out.println("D days"+"="+i/D);
-	         	System.out.println("H hours"+"="+i/H);
-	         	System.out.println("M minutes"+"="+i/M);
-	         	System.out.println("S seconds"+"="+i/S);
-	         	
-						
-		}
-}
+		int a = 6;
+		int b = 3; 
+		
+		System.out.printf("%d + %d = %d%n", a, b, a + b); 
+					
+       } 
+    }
